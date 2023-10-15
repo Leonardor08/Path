623a263f1d66b12aea95ea7ba26d04bbfe08a7ae
Leonardor08 <leonardor.dev08@gmail.com>
2023-10-15 17:29:23 -0500

    correción carpetas y orden

---
+++ Clases y Objetos/Csharp/Person.cs
@@ -0,0 +1,8 @@
+﻿using Module_1;
+
+// Creación de Objetos
+Person person = new("Curtis", 25);
+Person anotherPerson = new("Estefan", 32);
+// Llamado de Objetos internos de la clase
+person.ImprimirValores();
+anotherPerson.ImprimirValores();
---
+++ Clases y Objetos/Csharp/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_1
+{
+    //Definición de la Clase Person
+    public class Person
+    {
+#nullable disable
+        public string Name { get; set; }
+        public int Age { get; set; }
+        //Se declara el construtor de la clase
+        public Person(string name, int age) => (Name, Age) = (name, age); 
+        //Método para imprimir los valores
+        public void ImprimirValores() => Console.WriteLine($"Hola, soy {Name}, y tengo {Age}");
+    }
+}
