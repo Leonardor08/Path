3e7e64b80343d6ef6467d333dc2d3637476cc0ee
Leonardor08 <leonardor.dev08@gmail.com>
2023-10-16 14:59:44 -0500

    Deconstruct
    Uso método deconstruct

---
+++ Atributos y Métodos/Csharp/Deconstruct/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Deconstruct
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age) => (Name, Age) = (name, age);
+
+        public void Deconstruct(out string name, out int age)
+        {
+            name = Name;
+            age = Age;
+        }
+    }
+}
---
+++ Atributos y Métodos/Csharp/Deconstruct/Program.cs
@@ -0,0 +1,5 @@
+﻿using Deconstruct;
+
+Person person = new("Claudia", 13);
+(string name, int age) = person;
+Console.WriteLine($"Nombre: {name}, Edad {age}");
